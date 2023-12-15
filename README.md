ea0868643d374a8f12c3159087bb2c92e634c85a
kate2610k <ekaterinakarpova2610@gmail.com>
2023-12-15 14:44:02 +1000

    Step 7

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/9f1ad25c3ca636f85337/maintainability)](https://codeclimate.com/github/kate2610k/php-project-45/maintainability)
 [![asciicast](https://asciinema.org/a/mCXDEayad2OdKSNz72pqk1Xe5.svg)](https://asciinema.org/a/mCXDEayad2OdKSNz72pqk1Xe5)
 [![asciicast](https://asciinema.org/a/rCqejQ5EBgPGygXnc4MGMmJPO.svg)](https://asciinema.org/a/rCqejQ5EBgPGygXnc4MGMmJPO)
+[![asciicast](https://asciinema.org/a/KMd7Lb1Znb5osznEzBT4IqRNx.svg)](https://asciinema.org/a/KMd7Lb1Znb5osznEzBT4IqRNx)
--- src/Engine.php
+++ src/Engine.php
@@ -11,20 +11,18 @@ function start($challenge, $questionAll, $answerAll)
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
     line($challenge);
-
     $count = 0;
     while ($count <= 3) {
         if ($count === 3) {
             line("Congratulations, %s!", $name);
             break;
         }
-
         $answer = prompt('Question: ' . $questionAll[$count]);
         if ($answer == $answerAll[$count]) {
             $count += 1;
             line('Correct!');
         } else {
-            line($answer . "  is wrong answer ;(. Correct answer was " . $answerAll[$count]);
+            line("'" . $answer . "' is wrong answer ;(. Correct answer was '" . $answerAll[$count] . "'.");
             line("Let's try again, %s!", $name);
             break;
         }
